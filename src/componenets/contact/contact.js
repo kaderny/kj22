ca1b2cbedb768ad66cebd30e0f68b62b6e5f05ab
kaderny <kaderbrklyn@outlook.com>
2023-02-02 01:23:33 -0600

    contact lorem test

--- src/componenets/contact/contact.js
+++ src/componenets/contact/contact.js
@@ -5,6 +5,12 @@ function contact() {
   return (
     <div className="contact" id="contact">
       <Heading name="Contact" />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+        veritatis iusto reiciendis laboriosam quaerat provident architecto
+        expedita, aliquid consectetur in dicta excepturi impedit harum quia,
+        earum qui suscipit! Iste, laudantium.
+      </div>
     </div>
   );
 }
